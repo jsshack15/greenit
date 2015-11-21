e7bb3d6dfc02578b590757383da6b0829168ee55
Neha Gupta <gneha.bob@gmail.com>
2015-11-21 17:29:10 +0530

    First Commit

---
+++ java/com/sdcards/greenit/greenit/EducationFragment.java
@@ -0,0 +1,34 @@
+package com.sdcards.greenit.greenit;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EducationFragment extends Fragment {
+
+
+    public EducationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_education, container, false);
+    }
+
+    public void literacyButtonClicked(View view){
+        startActivity(new Intent(view.getContext(), LiteracyActivity.class));
+    }
+
+}
---
+++ java/com/sdcards/greenit/greenit/HousesFragment.java
@@ -0,0 +1,30 @@
+package com.sdcards.greenit.greenit;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HousesFragment extends Fragment {
+
+
+    public HousesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_houses, container, false);
+    }
+
+
+}

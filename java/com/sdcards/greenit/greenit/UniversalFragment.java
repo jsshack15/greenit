292a655c51a88da07ac7e10fcd3b3eadc9dcb64c
Neha Gupta <gneha.bob@gmail.com>
2015-11-22 05:50:30 +0530

    Fourth Commit

--- java/com/sdcards/greenit/greenit/UniversalFragment.java
+++ java/com/sdcards/greenit/greenit/UniversalFragment.java
@@ -1,11 +1,13 @@
 package com.sdcards.greenit.greenit;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -22,8 +24,53 @@ public class UniversalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_universal, container, false);
+        View view=inflater.inflate(R.layout.fragment_universal, container, false);
+
+        Button structButton = (Button) view.findViewById(R.id.structButton);
+
+        structButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(v.getContext(), HealthInfraActivity.class));
+                    }
+                }
+        );
+
+        Button enefButton = (Button) view.findViewById(R.id.enefButton);
+
+        enefButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(v.getContext(), EnefActivity.class));
+                    }
+                }
+        );
+
+        Button landButton = (Button) view.findViewById(R.id.landButton);
+
+        landButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(v.getContext(), BioWasteActivity.class));
+                    }
+                }
+        );
+
+        Button householdButton = (Button) view.findViewById(R.id.householdButton);
+
+        householdButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(v.getContext(), MedDeviceActivity.class));
+                    }
+                }
+        );
+
+        return view;
     }
 
 
